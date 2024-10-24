bb7e7798b6c4773dd33c5e65f933d42fcba1518a
fnathyas <158866417+fnathyas@users.noreply.github.com>
2024-10-24 13:55:57 +0700

    Add files via upload

---
+++ praktikum_9/MathUtils.java
@@ -0,0 +1,25 @@
+// ****************************************************************
+// MathUtils.java
+//
+// Provides static mathematical utility functions.
+//
+// ****************************************************************
+public class MathUtils{
+    //-------------------------------------------------------------
+// Returns the factorial of the argument given
+//-------------------------------------------------------------
+    public static int factorial(int n){
+        if (n < 0){
+            throw new IllegalArgumentException ("Untuk bilangan negatif faktorial tidak terdefinisi");
+        }
+
+        if (n > 16){
+            throw new IllegalArgumentException("Bilangan terlalu besar, nilai maksimal bilangan adalah 16");
+        }
+
+        int fac = 1;
+        for (int i=n; i>0; i--)
+            fac *= i;
+        return fac;
+    }
+}
